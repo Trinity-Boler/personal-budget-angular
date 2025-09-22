18821604d668e07f555013cdc3b54118ca1dc9f2
TTQBqueen <ttboler@uncg.edu>
2025-09-22 11:37:57 -0400

    got the nav links working

--- personal-budget/src/app/app.ts
+++ personal-budget/src/app/app.ts
@@ -1,15 +1,27 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { Menu } from './menu/menu';
 import { Hero } from './hero/hero';
 import { Footer } from './footer/footer';
 import { Article } from './article/article';
 import { Homepage } from './homepage/homepage';
-import { routes } from './app.routes';
+
+
 
 @Component({
   selector: 'pb-root',
-  imports: [RouterOutlet, Menu,Hero,Footer,Article,Homepage],
+  imports: [
+
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    Menu,
+    Hero,
+    Footer,
+    Article,
+    Homepage],
+
+
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- personal-budget/src/app/menu/menu.ts
+++ personal-budget/src/app/menu/menu.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'pb-menu',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu.html',
   styleUrl: './menu.scss'
 })
